5f9147c920a9274d12fc84427fdff1214903be48
Silvio Traversaro <silvio@traversaro.it>
2023-09-01 09:15:16 +0200

    Update test_resolve_robotics_uri_py.py

--- test/test_resolve_robotics_uri_py.py
+++ test/test_resolve_robotics_uri_py.py
@@ -2,5 +2,5 @@ import resolve_robotics_uri_py
 import pytest
 
 def test_non_existing_file():
-    with pytest.raises(MemoryError):
+    with pytest.raises(FileNotFoundError):
         resolve_robotics_uri_py.resolve_robotics_uri("package://this/package/and/file/does/not.exist")
